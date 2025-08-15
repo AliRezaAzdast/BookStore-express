76caa0e9a880aac9fb62652a531810dcecc4874e
AliRezaAzdast <151525801+AliRezaAzdast@users.noreply.github.com>
2025-08-15 21:59:48 +0330

    add few more api i most likly stop this project here since i learn most of the thing that i planned on mongoos

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,9 +26,71 @@ exports.deleteUser = async (req, res) => {
     if (!deletedUser) {
       return res.status(404).json({ message: "user dose not exist" });
     }
-  }else{
+  } else {
     return res.status(422).json({ message: "user id is not valid" });
   }
 
   res.status(202).json({ message: "user deleted" });
 };
+
+exports.crime = async (req, res) => {
+  const { id } = req.params;
+  const { crime } = req.body;
+  if (isValidObjectId(id)) {
+    const updateCrime = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          crime: crime,
+        },
+      }
+    );
+    if (!updateCrime) {
+      return res.status(404).json({ message: "user dose not exist" });
+    }
+  } else {
+    return res.status(422).json({ message: "user id is not valid" });
+  }
+  res.status(202).json({ message: "User crime updated" });
+};
+
+
+exports.makeAdmin = async (req, res) => {
+  const { id } = req.params;
+  if (isValidObjectId(id)) {
+    const findAndMadeAdmin = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          role: 'ADMIN'
+        },
+      }
+    );
+    if (!findAndMadeAdmin) {
+      return res.status(404).json({ message: "user dose not exist" });
+    }
+  } else {
+    return res.status(422).json({ message: "user id is not valid" });
+  }
+  res.status(202).json({ message: "User is admin now" });
+}
+
+exports.demoteFromAdmin = async (req, res) => {
+  const { id } = req.params;
+  if (isValidObjectId(id)) {
+    const findAndDemot = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          role: 'USER'
+        },
+      }
+    );
+    if (!findAndDemot) {
+      return res.status(404).json({ message: "user dose not exist" });
+    }
+  } else {
+    return res.status(422).json({ message: "user id is not valid" });
+  }
+  res.status(202).json({ message: "User is demoted now" });
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { getUsers, createUser, deleteUser } = require('../controllers/userController');
+const { getUsers, createUser, deleteUser, crime, makeAdmin, demoteFromAdmin } = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/', createUser);
 router.delete('/:id', deleteUser);
+router.put('/crime/:id', crime);
+router.put('/makeAdmin/:id', makeAdmin);
+router.put('/demote/:id', demoteFromAdmin);
 
 module.exports = router;
